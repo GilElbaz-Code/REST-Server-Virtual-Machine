a2d322c60b7543df40016f16ac09feae33e4cc69
Gil <elbazg123@gmail.com>
2021-10-22 13:56:42 +0300

    update 2

--- v1.py
+++ v1.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 from flask_restful import Api, Resource
+from flask_profiler import Profiler, flask_profiler
 import json
+import time
 
 app = Flask(__name__)
-
+app.config['JSON_SORT_KEYS'] = False
 api = Api(app)
 
 virtual_machines = []
 firewall_rules = []
+diffs = []
+COUNT = 0
 
 
 # This function reads the json file and stores each element in the relevant list
@@ -18,8 +22,11 @@ def read_and_store():
     virtual_machines.append(json_dict['vms'][0])
     firewall_rules.append(json_dict['fw_rules'])
 
+
 class Attack(Resource):
     def get(self, vm_id):
+        global COUNT
+        COUNT += 1
         vm_tags = []
         fw_tags = []
         vm = next(filter(lambda x: x['vm_id'] == vm_id, virtual_machines), None)
@@ -34,7 +41,23 @@ class Attack(Resource):
 
 class Stats(Resource):
     def get(self):
-        return {"vm_count:": len(virtual_machines)}
+        global COUNT
+        COUNT += 1
+        return jsonify(vm_count=len(virtual_machines), request_count=COUNT,
+                       average_request_time=(sum(diffs) / len(diffs)))
+
+
+@app.before_request
+def before_request():
+    g.start = time.time()
+
+
+@app.after_request
+def after_request(response):
+    diff = time.time() - g.start
+    diffs.append(diff)
+    return response
+
 
 
 api.add_resource(Attack, '/attack', '/attack/<string:vm_id>')
@@ -42,6 +65,4 @@ api.add_resource(Stats, '/stats')
 
 if __name__ == '__main__':
     read_and_store()
-    # print(virtual_machines)
-    # print(firewall_rules)
     app.run(port=5000)
