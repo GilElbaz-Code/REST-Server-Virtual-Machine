87620cdce430606b078898c88f12c7a454fb9575
Gil <elbazg123@gmail.com>
2021-10-24 15:24:15 +0300

    update 2

--- v1.py
+++ v1.py
@@ -9,6 +9,7 @@ api = Api(app)
 
 virtual_machines = []
 firewall_rules = []
+vm_dict = {}
 TIME_SUM = 0
 COUNT = 0
 
@@ -18,52 +19,56 @@ def read_and_store():
     with open(r"C:\Users\Gil\PycharmProjects\api\data\input-0.json") as file:
         json_str = file.read()
         json_dict = json.loads(json_str)
-    virtual_machines.append(json_dict['vms'][0])
+    virtual_machines.append(json_dict['vms'])
     firewall_rules.append(json_dict['fw_rules'])
+    print(json_dict['vms'])
 
 
-class Attack(Resource):
-    def get(self, vm_id):
-        global COUNT
-        COUNT += 1
-        vm_tags = []
-        fw_tags = []
-        # Check if the vm exist in input
-        vm = next(filter(lambda x: x['vm_id'] == vm_id, virtual_machines), None)
-        for fw in firewall_rules:
-            pass
-        # Append all tags of found vm in a list
-        if vm is not None:
-            vm_tags.append(vm['tags'])
-            return vm_tags
-        else:
-            return "VM not found!"
+    class Attack(Resource):
+        def get(self, vm_id):
+            global COUNT
+            COUNT += 1
+            vm_tags = []
+            can_access = []
+            dest_tag = ""
+            src_tag = ""
+            # Check if the vm exist in input
+            vm = next(filter(lambda x: x['vm_id'] == vm_id, virtual_machines), None)
+            if vm is not None:
+                # Append all tags of found vm in a list
+                vm_tags.append(vm['tags'])
+            else:
+                return "VM not found!"
+            for fw in firewall_rules[0]:
+                if fw['dest_tag'] in vm_tags[0]:
+                    dest_tag = fw['dest_tag']
+                    src_tag = fw['source_tag']
+            for vm_acc in virtual_machines:
+                if src_tag in vm_acc['tags']:
+                    can_access.append(vm_acc['vm_id'])
+                return jsonify(can_access)
 
+    class Stats(Resource):
+        def get(self):
+            global COUNT
+            COUNT += 1
+            return jsonify(vm_count=len(virtual_machines), request_count=COUNT,
+                           average_request_time=(TIME_SUM / COUNT))
 
-class Stats(Resource):
-    def get(self):
-        global COUNT
-        COUNT += 1
-        return jsonify(vm_count=len(virtual_machines), request_count=COUNT,
-                       average_request_time=(TIME_SUM / COUNT))
+    @app.before_request
+    def before_request():
+        g.start = time.time()
 
+    @app.after_request
+    def after_request(response):
+        global TIME_SUM
+        diff = time.time() - g.start
+        TIME_SUM += diff
+        return response
 
-@app.before_request
-def before_request():
-    g.start = time.time()
+    api.add_resource(Attack, '/attack', '/attack/<string:vm_id>')
+    api.add_resource(Stats, '/stats')
 
-
-@app.after_request
-def after_request(response):
-    global TIME_SUM
-    diff = time.time() - g.start
-    TIME_SUM += diff
-    return response
-
-
-api.add_resource(Attack, '/attack', '/attack/<string:vm_id>')
-api.add_resource(Stats, '/stats')
-
-if __name__ == '__main__':
-    read_and_store()
-    app.run(port=5000)
+    if __name__ == '__main__':
+        read_and_store()
+        app.run(port=5000)
